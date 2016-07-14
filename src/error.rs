2648cfc81c8dd101a62a850b2e7408a2571ebcd9
moreylx <moreylx@users.noreply.github.com>
2016-07-14 15:18:51 +0000

    add NtStatus wrapper

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,23 @@
+use std::fmt;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct NtStatus(pub i32);
 
 impl NtStatus {
+    pub const SUCCESS: Self = Self(0);
+    
     pub fn is_success(&self) -> bool {
         self.0 >= 0
+    }
+    
+    pub fn is_error(&self) -> bool {
+        self.0 < 0
+    }
+}
+
+impl fmt::Debug for NtStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "NtStatus(0x{:08X})", self.0 as u32)
     }
 }
 
