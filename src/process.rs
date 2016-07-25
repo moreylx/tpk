4cfd97fbb86c98c89d820859d2edf26a7b240f7f
moreylx <moreylx@users.noreply.github.com>
2016-07-25 18:06:12 +0000

    implement Process::open

--- src/process.rs
+++ src/process.rs
@@ -1,4 +1,6 @@
+use crate::error::{Result, NtStatus};
 use crate::handle::Handle;
+use windows::Win32::Foundation::HANDLE;
 use windows::Win32::System::Threading::*;
 
 pub struct Process {
@@ -13,4 +15,15 @@ impl Process {
             pid: unsafe { GetCurrentProcessId() },
         }
     }
+    
+    pub fn open(pid: u32, access: u32) -> Result<Self> {
+        let handle = unsafe { OpenProcess(PROCESS_ACCESS_RIGHTS(access), false, pid) };
+        match handle {
+            Ok(h) => Ok(Self { handle: Handle::new(h), pid }),
+            Err(_) => Err(NtStatus(-1)),
+        }
+    }
+    
+    pub fn handle(&self) -> HANDLE { self.handle.raw() }
+    pub fn pid(&self) -> u32 { self.pid }
 }
