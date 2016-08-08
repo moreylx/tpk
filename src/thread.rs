7004622d24c279fb80a116cbef2063d8c849db9c
moreylx <moreylx@users.noreply.github.com>
2016-08-08 22:07:28 +0000

    implement suspend/resume

--- src/thread.rs
+++ src/thread.rs
@@ -1,3 +1,4 @@
+use crate::error::{Result, NtStatus};
 use crate::handle::Handle;
 use windows::Win32::Foundation::HANDLE;
 use windows::Win32::System::Threading::*;
@@ -13,5 +14,23 @@ impl Thread {
             handle: Handle::current_thread(),
             tid: unsafe { GetCurrentThreadId() },
         }
+    }
+    
+    pub fn open(tid: u32, access: u32) -> Result<Self> {
+        let handle = unsafe { OpenThread(THREAD_ACCESS_RIGHTS(access), false, tid) };
+        match handle {
+            Ok(h) => Ok(Self { handle: Handle::new(h), tid }),
+            Err(_) => Err(NtStatus(-1)),
+        }
+    }
+    
+    pub fn suspend(&self) -> Result<u32> {
+        let result = unsafe { SuspendThread(self.handle.raw()) };
+        if result != u32::MAX { Ok(result) } else { Err(NtStatus(-1)) }
+    }
+    
+    pub fn resume(&self) -> Result<u32> {
+        let result = unsafe { ResumeThread(self.handle.raw()) };
+        if result != u32::MAX { Ok(result) } else { Err(NtStatus(-1)) }
     }
 }
