0d82f7d02c06ef9acc20f5e3c4ef731ba7a26494
moreylx <moreylx@users.noreply.github.com>
2016-08-25 21:10:58 +0000

    implement VirtualAlloc

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,5 @@
+use crate::error::{Result, NtStatus};
+use windows::Win32::Foundation::HANDLE;
 use windows::Win32::System::Memory::*;
 use std::ffi::c_void;
 
@@ -6,4 +8,30 @@ pub struct Protection(pub u32);
 impl Protection {
     pub fn readwrite() -> Self { Self(PAGE_READWRITE.0) }
     pub fn execute_read() -> Self { Self(PAGE_EXECUTE_READ.0) }
+}
+
+pub struct VirtualAlloc {
+    base: *mut c_void,
+    size: usize,
+    handle: HANDLE,
+}
+
+impl VirtualAlloc {
+    pub fn allocate(handle: HANDLE, size: usize, prot: Protection) -> Result<Self> {
+        let base = unsafe {
+            VirtualAllocEx(handle, None, size, MEM_COMMIT | MEM_RESERVE, 
+                PAGE_PROTECTION_FLAGS(prot.0))
+        };
+        if base.is_null() { Err(NtStatus(-1)) } else { Ok(Self { base, size, handle }) }
+    }
+    
+    pub fn base(&self) -> *mut c_void { self.base }
+}
+
+impl Drop for VirtualAlloc {
+    fn drop(&mut self) {
+        if !self.base.is_null() {
+            unsafe { let _ = VirtualFreeEx(self.handle, self.base, 0, MEM_RELEASE); }
+        }
+    }
 }
