978e9d5ca9ff85f122462d2678884cd0b4563fda
moreylx <moreylx@users.noreply.github.com>
2016-08-29 17:11:03 +0000

    update lib exports

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,9 @@ pub mod error;
 pub mod handle;
 pub mod process;
 pub mod thread;
+pub mod memory;
+
+pub use error::{NtStatus, Result};
+pub use handle::Handle;
+pub use process::Process;
+pub use thread::Thread;
