fc250de164d42c18de599be289c5d96cb077f1dc
moreylx <moreylx@users.noreply.github.com>
2021-11-23 18:17:40 +0000

    finalize lib exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+
 pub mod error;
-pub mod handle;
 pub mod process;
 pub mod thread;
 pub mod memory;
+pub mod handle;
 
 pub use error::{NtStatus, Result};
-pub use handle::Handle;
 pub use process::Process;
 pub use thread::Thread;
+pub use handle::Handle;
+
