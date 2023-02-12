9c40720528424fd9d5672ff3a011bca408ca2514
moreylx <moreylx@users.noreply.github.com>
2023-02-12 11:19:46 +0000

    add error Display impl

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use windows::Win32::Foundation::NTSTATUS;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct NtStatus(pub i32);
@@ -13,6 +14,10 @@ impl NtStatus {
     pub fn is_error(&self) -> bool {
         self.0 < 0
     }
+    
+    pub fn from_ntstatus(status: NTSTATUS) -> Self {
+        Self(status.0)
+    }
 }
 
 impl fmt::Debug for NtStatus {
@@ -21,4 +26,26 @@ impl fmt::Debug for NtStatus {
     }
 }
 
+impl fmt::Display for NtStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.is_success() {
+            write!(f, "Success")
+        } else {
+            write!(f, "Error: 0x{:08X}", self.0 as u32)
+        }
+    }
+}
+
+impl std::error::Error for NtStatus {}
+
 pub type Result<T> = std::result::Result<T, NtStatus>;
+
+pub fn check_status(status: NTSTATUS) -> Result<()> {
+    let s = NtStatus::from_ntstatus(status);
+    if s.is_success() {
+        Ok(())
+    } else {
+        Err(s)
+    }
+}
+
